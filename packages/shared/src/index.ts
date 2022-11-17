f588a4d1f8d05bae10752088ab33a093a013da4e
yunumu <1416900346@qq.com>
2022-11-17 23:43:42 +0800

    初始化项目结构

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,2 @@
+// 判断数组
+export const isArray = Array.isArray
---
+++ packages/vue/dist/vue.js
@@ -0,0 +1,9 @@
+;(function () {
+  'use strict'
+
+  // 判断数组
+  var isArray = Array.isArray
+
+  console.log(isArray({}))
+})()
+//# sourceMappingURL=vue.js.map
