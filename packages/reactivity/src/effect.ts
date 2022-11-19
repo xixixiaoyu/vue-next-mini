6bb06836591db0217f51355ba9508a7d09eb1c9a
yunumu <1416900346@qq.com>
2022-11-19 10:24:37 +0800

    构建 track 依赖收集函数

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,24 +1,4 @@
 /**
- * 用于收集依赖的方法
- * @param target WeakMap 的 key
- * @param key 代理对象的 key，当依赖被触发时，需要根据该 key 获取
- */
-export function track(target: object, key: unknown) {
-  console.log('track: 收集依赖')
-}
-
-/**
- * 触发依赖的方法
- * @param target WeakMap 的 key
- * @param key 代理对象的 key，当依赖被触发时，需要根据该 key 获取
- * @param newValue 指定 key 的最新值
- * @param oldValue 指定 key 的旧值
- */
-export function trigger(target: object, key?: unknown, newValue?: unknown) {
-  console.log('trigger: 触发依赖')
-}
-
-/**
  * effect 函数
  * @param fn 执行方法
  * @returns 以 ReactiveEffect 实例为 this 的 fn 执行函数
@@ -42,4 +22,48 @@ export class ReactiveEffect<T = any> {
     // 执行effect传入函数
     return this.fn()
   }
+}
+
+type KeyToDepMap = Map<any, ReactiveEffect>
+/**
+ * 收集所有依赖的 WeakMap 实例：
+ * 1. `key`：响应性对象
+ * 2. `value`：`Map` 对象
+ * 		1. `key`：响应性对象的指定属性
+ * 		2. `value`：指定对象的指定属性的 执行函数
+ */
+const targetMap = new WeakMap<any, KeyToDepMap>()
+
+/**
+ * 用于收集依赖的方法
+ * @param target WeakMap 的 key
+ * @param key 代理对象的 key，当依赖被触发时，需要根据该 key 获取
+ */
+export function track(target: object, key: unknown) {
+  // 当前不存在执行函数直接return
+  if (!activeEffect) {
+    return
+  }
+  // 根据new Proxy传入对象从targetMap取值
+  let depsMap = targetMap.get(target)
+
+  // 没有值，则构造外层结构映射关系
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+  // 设置访问的key和activeEffect实例映射关系
+  depsMap.set(key, activeEffect)
+
+  console.log('targetMap', targetMap)
+}
+
+/**
+ * 触发依赖的方法
+ * @param target WeakMap 的 key
+ * @param key 代理对象的 key，当依赖被触发时，需要根据该 key 获取
+ * @param newValue 指定 key 的最新值
+ * @param oldValue 指定 key 的旧值
+ */
+export function trigger(target: object, key?: unknown, newValue?: unknown) {
+  console.log('trigger: 触发依赖')
 }
