7b8d88208ca2630e5a9b294f81058edccefaa9bb
yunumu <1416900346@qq.com>
2022-11-19 21:21:55 +0800

    ref 函数 - 构建简单数据类型的响应性

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,7 +1,8 @@
 import type { Dep } from './dep'
-import { activeEffect, trackEffects } from './effect'
+import { activeEffect, trackEffects, triggerEffects } from './effect'
 import { createDep } from './dep'
 import { toReactive } from './reactive'
+import { hasChanged } from '@vue/shared'
 
 export interface Ref<T = any> {
   value: T
@@ -27,14 +28,16 @@ function createRef(rawValue: unknown, shallow: boolean) {
 
 class RefImpl<T> {
   private _value: T
+  private _rawValue: T
   public dep?: Dep = undefined
-
   // 是否为 ref 类型数据的标记
   public readonly __v_isRef = true
 
   constructor(value: T, public readonly __v_isShallow: boolean) {
     // 如果__v_isShallow为true，则不会转换为响应式数据
     this._value = __v_isShallow ? value : toReactive(value)
+    // 原始数据
+    this._rawValue = value
   }
 
   get value() {
@@ -42,7 +45,16 @@ class RefImpl<T> {
     return this._value
   }
 
-  set value(newValue) {}
+  set value(newValue) {
+    if (hasChanged(this._rawValue, newValue)) {
+      // 更新原始数据
+      this._rawValue = newValue
+      // 更新.value的值
+      this._value = toReactive(newValue)
+      // 触发依赖
+      triggerRefValue(this)
+    }
+  }
 }
 
 // 指定数据是否是 RefImpl 类型
@@ -54,5 +66,12 @@ function isRef(r: any): r is Ref {
 function trackRefValue(ref) {
   if (activeEffect) {
     trackEffects(ref.dep || (ref.dep = createDep()))
+  }
+}
+
+//  为 ref 的 value 进行触发依赖工作
+function triggerRefValue(ref) {
+  if (ref.dep) {
+    triggerEffects(ref.dep)
   }
 }
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -4,3 +4,7 @@ export const isArray = Array.isArray
 // 判断是否对象
 export const isObject = (val: unknown) =>
   val !== null && typeof val === 'object'
+
+// 判断是否变化
+export const hasChanged = (value: any, oldValue: any) =>
+  !Object.is(value, oldValue)
