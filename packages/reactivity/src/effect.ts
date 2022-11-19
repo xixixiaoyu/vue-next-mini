9668f147c14dddec635317b0cba1bd99fd16b4d8
yunumu <1416900346@qq.com>
2022-11-19 09:58:46 +0800

    构建 effect 函数，生成 ReactiveEffect 实例

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -17,3 +17,29 @@ export function track(target: object, key: unknown) {
 export function trigger(target: object, key?: unknown, newValue?: unknown) {
   console.log('trigger: 触发依赖')
 }
+
+/**
+ * effect 函数
+ * @param fn 执行方法
+ * @returns 以 ReactiveEffect 实例为 this 的 fn 执行函数
+ */
+export function effect<T = any>(fn: () => T) {
+  // 生成ReactiveEffect实例
+  const _effect = new ReactiveEffect(fn)
+  // 执行实例run方法
+  _effect.run()
+}
+
+// 单例的，当前的 effect
+export let activeEffect: ReactiveEffect | undefined
+
+// 响应性触发依赖时的执行类
+export class ReactiveEffect<T = any> {
+  constructor(public fn: () => T) {}
+  run() {
+    // activeEffect赋值当前实例
+    activeEffect = this
+    // 执行effect传入函数
+    return this.fn()
+  }
+}
