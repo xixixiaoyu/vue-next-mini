24810b903487f1ea0d5114c7533a717d9159d98c
yunumu <1416900346@qq.com>
2023-02-21 00:53:18 +0800

    完成无状态基础组件的挂载逻辑

--- packages/runtime-core/src/componentRenderUtils.ts
+++ packages/runtime-core/src/componentRenderUtils.ts
@@ -1,4 +1,25 @@
+import { ShapeFlags } from 'packages/shared/src/shapeFlag'
 import { createVNode } from './vnode'
+
+/**
+ * 解析 render 函数的返回值
+ */
+export function renderComponentRoot(instance) {
+  const { vnode, render } = instance
+
+  let result
+  try {
+    // 解析到状态组件
+    if (vnode.shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
+      // 获取到 result 返回值
+      result = normalizeVNode(render!())
+    }
+  } catch (err) {
+    console.error(err)
+  }
+
+  return result
+}
 
 /**
  * 标准化 VNode
